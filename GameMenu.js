e6632d030fc9fa70312ff03d312ade9ba6ead60e
Samuel Antonio Esquivel <missionbit@Missions-MacBook-Pro-90.local>
2017-04-27 17:41:54 -0700

    lelelel

--- GameMenu.js
+++ GameMenu.js
@@ -1,37 +1,42 @@
-//var GameMenu = function () {};
+var GameMenu = function() {};
 
-GameMenu.prototype = {   
-menuConfig: {
+
+GameMenu.prototype = {
+
+  menuConfig: {
     startY: 260,
-    startx: 30
-},
-    
-//innit: function () {
-    this.titletext = game.make.text(game.world.centerx, 100, "Game Title", {
-        font: 'bold 60pt TheMinion',
-        fill: '#FDFFB5',
-        align: 'center'  } ),
-        
-    
-};
+    startX: 30
+  },
 
-//create: function () {
-    
-    game.stage.DisableVisibilityChange = true;
-    game.add.sprite(0,0, 'menu-bg');
-    game.add.existing(this.titletext);
-    
+  init: function () {
+    this.titleText = game.make.text(game.world.centerX, 100, "Game Title", {
+      font: 'bold 60pt Arial',
+      fill: '#FDFFB5',
+      align: 'center'
+    });
+    this.titleText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
+    this.titleText.anchor.set(0.5);
+    this.optionCount = 1;
+  },
+
+  create: function () {
+
+    if 
+    }
+    game.stage.disableVisibilityChange = true;
+    game.add.sprite(0, 0, 'menu-bg');
+    game.add.existing(this.titleText);
+
     this.addMenuOption('Start', function () {
-        game.add.start("Game");
+      game.state.start("Game");
     });
     this.addMenuOption('Options', function () {
-        game.add.start("Options")
-    } );
-    this.addMenuOption('Credits', function () {
-     game.state.start("Credits");   
+      game.state.start("Options");
     });
-                       
-                      
-                      
-                      )
-}
+    this.addMenuOption('Credits', function () {
+      game.state.start("Credits");
+    });
+  }
+};
+
+Phaser.Utils.mixinPrototype(GameMenu.prototype, mixins);
